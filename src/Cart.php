e1ea8d210c8e73ff2a263900ff28f734f9e61143
Marc Serra <mserra@softwaremakers.es>
2020-08-31 13:59:32 +0200

    Persist discount id

--- src/Cart.php
+++ src/Cart.php
@@ -863,6 +863,7 @@ class Cart
         $this->createdAt = Carbon::parse(data_get($stored, 'created_at'));
         $this->updatedAt = Carbon::parse(data_get($stored, 'updated_at'));
         $this->userId = data_get($stored, 'user_id');
+        $this->discountId = data_get($stored, 'discount_id');
 
         $this->getConnection()->table($this->getTableName())->where('identifier', $identifier)->delete();
     }
@@ -908,6 +909,7 @@ class Cart
         $this->createdAt = Carbon::parse(data_get($stored, 'created_at'));
         $this->updatedAt = Carbon::parse(data_get($stored, 'updated_at'));
         $this->userId = data_get($stored, 'user_id');
+        $this->discountId = data_get($stored, 'discount_id');
     }
 
     /**
